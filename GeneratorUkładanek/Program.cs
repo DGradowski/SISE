75d7509d803cfb3d70aa67412f3d0e55d9b9f04a
Dawid Gradowski <251524@edu.p.lodz.pl>
2025-04-09 13:48:32 +0200

    Improve GeneratorUkładanek

--- GeneratorUkładanek/Program.cs
+++ GeneratorUkładanek/Program.cs
@@ -2,6 +2,7 @@
 using System.Reflection.PortableExecutable;
 using System.Xml;
 using System.Security.Cryptography;
+using Puzzle;
 
 public class MainClass
 {
@@ -10,111 +11,104 @@ public class MainClass
 		int rows = 0; // 0
 		int cols = 0; // 1
 		int depth = 0; // 2
-		string file = args[3]; // 3
 		Int32.TryParse(args[0], out rows);
 		Int32.TryParse(args[1], out cols);
 		Int32.TryParse(args[2], out depth);
-		HashSet<string> visited = new HashSet<string>();
+		Dictionary<string, PuzzleState> saved = new Dictionary<string, PuzzleState>();
+		Queue<PuzzleState> queue = new Queue<PuzzleState>();
 		Random r = new Random();
 		int zRow = rows - 1, zCol = cols - 1;
+		int solutionDepth = 0;
 
 
-		int[,] board = new int[rows, cols];
+		PuzzleState state = new PuzzleState(rows, cols);
 		for (int row = 0; row < rows; row++)
 		{
 			for (int col = 0; col < cols; col++)
 			{
 				if (row == rows - 1 && col == cols - 1)
 				{
-					board[row, col] = 0;
+					state.SetCell(row, col, 0);
 					break;
 				}
-				board[row, col] = (row * cols) + col + 1;
+				state.SetCell(row, col, (row * cols) + col + 1);
 			}
 		}
-		for (int i = 0; i < depth; i++)
+		queue.Enqueue(state);
+
+		while (solutionDepth <= depth)
 		{
-			visited.Add(BoardToKey(board));
-			bool boardChanged = false;
-			while (!boardChanged)
+			state = queue.Dequeue();
+			solutionDepth = state.Moves.Length;
+			if (saved.ContainsKey(state.BoardToKey())) continue;
+			foreach (char c in "LDRU")
 			{
-				int x = r.Next(4);
-				switch (x)
+				PuzzleState nState;
+				switch (c)
 				{
-					case 0:
+					case 'U':
 						// do góry
-						if (zRow == 0) break;
-						(board[zRow, zCol], board[zRow - 1, zCol]) = (board[zRow - 1, zCol], board[zRow, zCol]);
-						if (visited.Contains(BoardToKey(board)))
-						{
-							(board[zRow, zCol], board[zRow - 1, zCol]) = (board[zRow - 1, zCol], board[zRow, zCol]);
-						}
-						else
-						{
-							boardChanged = true;
-							zRow--;
-						}
+						if (!state.IsMoveLegal(c)) break;
+						nState = new PuzzleState(state, c);
+						if (saved.ContainsKey(nState.BoardToKey())) break;
+						queue.Enqueue(nState);
 						break;
 
-					case 1:
+					case 'D':
 						// do dołu
-						if (zRow == rows - 1) break;
-						(board[zRow, zCol], board[zRow + 1, zCol]) = (board[zRow + 1, zCol], board[zRow, zCol]);
-						if (visited.Contains(BoardToKey(board)))
-						{
-							(board[zRow, zCol], board[zRow + 1, zCol]) = (board[zRow + 1, zCol], board[zRow, zCol]);
-						}
-						else
-						{
-							boardChanged = true;
-							zRow++;
-						}
+						if (!state.IsMoveLegal(c)) break;
+						nState = new PuzzleState(state, c);
+						if (saved.ContainsKey(nState.BoardToKey())) break;
+						queue.Enqueue(nState);
 						break;
 
-					case 2:
+					case 'L':
 						// w lewo
-						if (zCol == 0) break;
-						(board[zRow, zCol], board[zRow, zCol - 1]) = (board[zRow, zCol - 1], board[zRow, zCol]);
-						if (visited.Contains(BoardToKey(board)))
-						{
-							(board[zRow, zCol], board[zRow, zCol - 1]) = (board[zRow, zCol - 1], board[zRow, zCol]);
-						}
-						else
-						{
-							boardChanged = true;
-							zCol--;
-						}
+						if (!state.IsMoveLegal(c)) break;
+						nState = new PuzzleState(state, c);
+						if (saved.ContainsKey(nState.BoardToKey())) break;
+						queue.Enqueue(nState);
 						break;
 
 					default:
 						// w prawo
-						if (zCol == cols - 1) break;
-						(board[zRow, zCol], board[zRow, zCol + 1]) = (board[zRow, zCol + 1], board[zRow, zCol]);
-						if (visited.Contains(BoardToKey(board)))
-						{
-							(board[zRow, zCol], board[zRow, zCol + 1]) = (board[zRow, zCol + 1], board[zRow, zCol]);
-						}
-						else
-						{
-							boardChanged = true;
-							zCol++;
-						}
+						if (!state.IsMoveLegal(c)) break;
+						nState = new PuzzleState(state, c);
+						if (saved.ContainsKey(nState.BoardToKey())) break;
+						queue.Enqueue(nState);
 						break;
 				}
 			}
+			if (state.Moves.Length > depth) break;
+			saved.Add(state.BoardToKey(), state);
 		}
 
-		String[] lines = new String[rows];
-		for (int i = 0; i < board.GetLength(0); i++)
+		Dictionary<int, int> depthId = new Dictionary<int, int>();
+
+		foreach (var k in saved.Keys.ToArray<string>())
 		{
-			lines[i] = "";
-			for (int j = 0; j < board.GetLength(1); j++)
+			if (saved[k].Moves.Length == 0) continue;
+			int d = saved[k].Moves.Length;
+			if (depthId.ContainsKey(d))
 			{
-				lines[i] += board[i, j].ToString() + " ";
+				depthId[d]++;
 			}
+			else
+			{
+				depthId.Add(d, 1);
+			}
+			String file = rows.ToString() + "x" + cols.ToString() + "_" + d.ToString("00") + "_" + depthId[d].ToString("00000") + ".txt";
+			String[] lines = new String[rows];
+			for (int i = 0; i < rows; i++)
+			{
+				lines[i] = "";
+				for (int j = 0; j < cols; j++)
+				{
+					lines[i] += saved[k].GetCell(i, j).ToString() + " ";
+				}
+			}
+			SaveToFile(file, rows, cols, lines);
 		}
-
-		SaveToFile(file, rows, cols, lines);
 	}
 
 
---
+++ GeneratorUkładanek/PuzzleState.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puzzle
+{
+	public class PuzzleState
+	{
+		private String moves;
+		private int[,] board;
+		private int zeroRow = 0;
+		private int zeroCol = 0;
+
+		public PuzzleState(int rows, int columns, String prevMoves = "")
+		{
+			board = new int[rows, columns];
+			moves = "";
+		}
+
+		public PuzzleState(PuzzleState state, char? move = null)
+		{
+			board = new int[state.Rows, state.Columns];
+			for (int i = 0; i < state.Rows; i++)
+			{
+				for (int j = 0; j < state.Columns; j++)
+				{
+					board[i,j] = state.GetCell(i, j);
+				}
+			}
+			moves = state.Moves + move;
+			if (move != null) MakeMove((char) move);
+		}
+
+		public int Rows => board.GetLength(0);
+		public int Columns => board.GetLength(1);
+		public string Moves => moves;
+
+		public int[,] Board => board;
+		public int GetCell(int row, int column) { return board[row,column]; }
+		public void SetCell(int row, int column, int value) { board[row,column] = value; }
+
+
+		public bool IsSolved()
+		{
+			for (int i = 0; i < Rows; i++)
+			{
+				for(int j = 0; j < Columns; j++)
+				{
+					if (i == Rows - 1 && j == Columns - 1)
+					{
+						if (board[i,j] != 0) return false;
+					}
+					else
+					{
+						if (board[i, j] != i * Columns + j + 1) return false;
+					}
+				}
+			}
+			return true;
+		}
+		public bool IsMoveLegal(char move)
+		{
+			FindZero();
+			if (zeroCol == 0 && move == 'L') return false;
+			if (zeroCol == Columns - 1 && move == 'R') return false;
+			if (zeroRow == 0 && move == 'U') return false;
+			if (zeroRow == Rows - 1 && move == 'D') return false;
+			return true;
+		}
+
+		public void MakeMove(char move)
+		{
+			if (!IsMoveLegal(move)) throw new Exception("Trying to make illegal move");
+			FindZero();
+			switch (move)
+			{
+				case 'L':
+					(board[zeroRow, zeroCol], board[zeroRow, zeroCol - 1]) = (board[zeroRow, zeroCol - 1], board[zeroRow, zeroCol]);
+					break;
+				case 'R':
+					(board[zeroRow, zeroCol], board[zeroRow, zeroCol + 1]) = (board[zeroRow, zeroCol + 1], board[zeroRow, zeroCol]);
+					break;
+				case 'U':
+					(board[zeroRow, zeroCol], board[zeroRow - 1, zeroCol]) = (board[zeroRow - 1, zeroCol], board[zeroRow, zeroCol]);
+					break;
+				case 'D':
+					(board[zeroRow, zeroCol], board[zeroRow + 1, zeroCol]) = (board[zeroRow + 1, zeroCol], board[zeroRow, zeroCol]);
+					break;
+			}
+		}
+
+		public void FindZero()
+		{
+			if (board[zeroRow, zeroCol] == 0) return;
+			for (int i = 0; i < Rows; i++)
+			{
+				for (int j = 0; j < Columns; j++)
+				{
+					if (board[i, j] == 0)
+					{
+						zeroRow = i;
+						zeroCol = j;
+						return;
+					}
+				}
+			}
+		}
+
+		public String BoardToKey()
+		{
+			String key = "";
+			for (int i = 0; i < Rows; i++)
+			{
+				for (int j = 0; j < Columns; j++)
+				{
+					key += board[i, j].ToString() + " ";
+				}
+			}
+			return key;
+		}
+
+		public bool CompareBoard(PuzzleState s)
+		{
+			if (s.Rows != Rows) return false;
+			if (s.Columns != Columns) return false;
+			for (int i = 0;i < Rows; i++)
+			{
+				for (int j = 0; j < Columns; j++)
+				{
+					if (s.GetCell(i, j) !=  GetCell(i, j)) return false;
+				}
+			}
+			return true;
+		}
+	}
+}
